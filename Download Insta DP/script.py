4c7e46a38e2962cfb1a35380441d05db57c7b536
Sagar Dutta <sagar0dutta@gmail.com>
2019-11-01 00:32:36 +0530

    a1

---
+++ Download Insta DP/script.py
@@ -0,0 +1,12 @@
+
+#import the necessary module!
+import instaloader
+
+#create an instance
+test = instaloader.Instaloader()
+
+#fetch the account details
+acc = input('Enter the Account Username: ')
+
+#download the data
+test.download_profile(acc, profile_pic_only = True)        
