3b0305b0c252be1c8e5eba5f83796db954dd3556
Sagar Dutta <sagar0dutta@gmail.com>
2020-08-09 03:21:36 +0530

    a1

---
+++ Unzip a File/source_code.py
@@ -0,0 +1,18 @@
+#import the necessary modules!
+import zipfile as z
+
+#set the target file
+target = 'demo.zip'
+
+#display a start message
+print('Starting to Unzip the File!')
+
+#use ZipFile method
+root = z.ZipFile(target)
+
+#give destination path
+root.extractall('new')
+root.close()
+
+#display the end message
+print('\nFile is Succesfully Unzipped!')
