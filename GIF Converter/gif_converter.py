8663303c94252fdc0663b0a45c7a010e2973e607
Sagar Dutta <sagar0dutta@gmail.com>
2020-08-14 12:23:06 +0530

    a1

---
+++ GIF Converter/gif_converter.py
@@ -0,0 +1,11 @@
+# import
+from moviepy.editor import *
+
+# load the video
+clip = VideoFileClip("my_video.mp4")
+
+# getting only 3 first seconds from video
+clip = clip.subclip(0, 3)
+
+# save the video clip as gif
+clip.write_gif("mygif.gif")
